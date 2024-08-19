16e6275b27b3ebc33bda4ca9c625b32c77a9b0a9
Camilo Galindo R <cgalindorivera@gmail.com>
2024-08-19 12:55:55 -0500

    updated order ms

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,25 +1,11 @@
-import {
-	IsBoolean,
-	IsEnum,
-	IsNumber,
-	IsOptional,
-	IsPositive,
-} from 'class-validator';
-import { OrderStatus, OrderStatusList } from '../enum/order.enum';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
+import { OrderItemDto } from './order-item.dto';
+import { Type } from 'class-transformer';
 
 export class CreateOrderDto {
-	@IsNumber()
-	@IsPositive()
-	public totalAmount: number;
-	@IsNumber()
-	@IsPositive()
-	public totalItems: number;
-	@IsEnum(OrderStatusList, {
-		message: 'Invalid status',
-	})
-	@IsOptional()
-	public status: OrderStatus = OrderStatus.PENDING;
-	@IsBoolean()
-	@IsOptional()
-	public paid: boolean = false;
+	@IsArray()
+	@ArrayMinSize(1)
+	@ValidateNested({ each: true })
+	@Type(() => OrderItemDto)
+	items: OrderItemDto[];
 }
